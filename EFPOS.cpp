3c49769be1f18fe2458e9017769b8699af56909c
Dimezske <dimezske@gmail.com>
2017-04-19 16:34:15 +1000

    The Main Source File.
    This is where all the functions of the program are run.

--- EFPOS.cpp
+++ EFPOS.cpp
@@ -26,81 +26,87 @@ class CreditCard
         char pin[5];
         unsigned int repNo = 0;
         
-        void printRecipt(){
-            while (repNo /= 10)
-            repNo += 3;    
-            std::time_t result = std::time(nullptr);
-            std::cout << "Time & Date         " << std::asctime(std::localtime(&result));
-            std::cout << "repNo:              " << std::setfill('0')<< std::setw(9) << repNo + rand() % 100 + 1 << std::endl;            
-        }
-        void Declined(){
-             std::cout << "DECLINED" << std::endl;
-        }
-        void Approved(){
-            std::cout << "APPROVED" << std::endl;
-            printRecipt();
-			itemName();
-        }
-       void showMoney(){
-            std::cout << money << std::endl;
-       }
-      void buyItem(){
-       money -= burger;
-       std::cout << "$15.00" << std::endl;
-       if (money < 0){
-       money += burger;
-       }
-       usleep(100000);
-       burgerCount++;
-       showMoney();
-        std::cout << "SALE: $" << 15.00 << std::endl;
-            if (money < 15.00){
-                Declined();
-            }
-            if (money >= 15.00){
-                Approved();
-            }
-      }
-       void itemName(){
-         std::cout << "Burger                     x" << burgerCount << std::endl;
-       }
-       void getMoney(){
-         money += 50.00;
-       }
+void printRecipt(){
+     while (repNo /= 10)
+     repNo += 3;    
+     std::time_t result = std::time(nullptr);
+     std::cout << "Time & Date         " << std::asctime(std::localtime(&result));
+     std::cout << "repNo:              " << std::setfill('0')<< std::setw(9) << repNo + rand() % 100 + 1 << std::endl;            
+     }
+	
+void Declined(){
+     std::cout << "DECLINED" << std::endl;
+     }
+	
+void Approved(){
+     std::cout << "APPROVED" << std::endl;
+     printRecipt(); 
+     itemName();
+     }
+	
+void showMoney(){
+     std::cout << money << std::endl;
+     }
+	
+void buyItem(){
+     money -= burger;
+     std::cout << "$15.00" << std::endl;
+     if (money < 0){
+         money += burger;
+         }      
+     usleep(100000);
+     burgerCount++;
+     showMoney();
+     std::cout << "SALE: $" << 15.00 << std::endl;
+     if (money < 15.00){
+         Declined();
+         }
+     if (money >= 15.00){
+         Approved();
+         }
+     }
+void itemName(){
+     std::cout << "Burger                     x" << burgerCount << std::endl;
+     }
+	
+void getMoney(){
+     money += 50.00;
+     }
 
-        void Purchase(){    
-            std::cout << "SALE: $" << 15.00 << std::endl;
-        }
-    void EnterCC(){
-            std::cout << "******|Enter the [16] Digit Creditcard Number|******" << std::endl;
-             std::cin.getline(ch,sizeof(ch)); 
-             std::cout << "CREDITCARD ENTERED" << std::endl;
-             usleep(100000);
-             std::cout << "Signalling CREDITCARD." << std::endl;
-             usleep(100000);
-             std::cout << "Signalling CREDITCARD.." << std::endl;
-             usleep(100000);
-             std::cout << "Signalling CREDITCARD..." << std::endl;
-             usleep(100000);
-	     for (i = 0;i < 15;i++){
-	 	 if (i == 4){
-                     std::cout << " ";
-                 }
-                 if (i == 7){
-                     std::cout << " ";
-                 }
-                 if (i == 11){
-                     std::cout << " ";
-                 }
-                 if (i == 15){
-                     std::cout << " ";
-                 }
-                 usleep(100000);
-                 std::cout << ch[i];
-                 std::cin.clear(); 
-           }
+void Purchase(){    
+     std::cout << "SALE: $" << 15.00 << std::endl;
+     }
+	
+void EnterCC(){
+     std::cout << "******|Enter the [16] Digit Creditcard Number|******" << std::endl;
+     std::cin.getline(ch,sizeof(ch)); 
+     std::cout << "CREDITCARD ENTERED" << std::endl
+     usleep(100000);
+     std::cout << "Signalling CREDITCARD." << std::endl;
+     usleep(100000);
+     std::cout << "Signalling CREDITCARD.." << std::endl;
+     usleep(100000);
+     std::cout << "Signalling CREDITCARD..." << std::endl;
+     usleep(100000);
+     for (i = 0;i < 15;i++){
+	 if (i == 4){
+            std::cout << " ";
+            }
+         if (i == 7){
+            std::cout << " ";
+            }
+         if (i == 11){
+            std::cout << " ";
+            }
+         if (i == 15){
+            std::cout << " "; 
+		}
+         usleep(100000);
+         std::cout << ch[i];
+         std::cin.clear(); 
+         }
     }
-	void EnterCSV(){
+void EnterCSV(){
          std::cout << std::endl;
          std::cout << "***Enter [3] Diigit CSV Number***" << std::endl;
          std::cout << "CSV# ";
